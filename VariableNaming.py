8a9110529126db461e603527c652fe3a561c0305
Dev-Archana <asm43636@gmail.com>
2024-08-12 12:10:53 +0530

    new commit

---
+++ VariableNaming.py
@@ -0,0 +1,24 @@
+""" 
+Unlike other programming language in python also we have some of the naming conventions for the variable 
+like here we have to give the name which are started from 
+a) alphabets from a to z (both capital and small case)
+b) digits from (0-9)
+c) underscore(_) is also allowed in the variable name
+d)if you are using _class and method name those are also known as important method or class
+e)__classname__ known as special method in python
+"""
+# Correct Variable Declaration 
+num1=10
+num2=20
+# Wrong Variable Declaration
+# 1num1=20
+# $num2=10
+
+# Correct Way
+_num9=10
+_num7_=8
+print(_num9)
+print(_num7_)
+
+#Printing the sum of two numbers
+print('Sum of two numbers',(num1+num2))
